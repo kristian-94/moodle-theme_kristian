9af28b2cc5b8f5e6f59c48ce6097346df2a470a7
Kristian Ringer <kristian.ringer@catalyst-au.net>
2020-06-07 12:37:15 +1000

    Initial skeleton files for theme

---
+++ lib.php
@@ -0,0 +1,8 @@
+<?php
+
+// Every file should have GPL and copyright in the header - we skip it in tutorials but you should not skip it for real.
+
+// This line protects the file from being accessed by a URL directly.
+defined('MOODLE_INTERNAL') || die();
+
+// We will add callbacks here as we add features to our theme.
